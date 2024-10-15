905abc179fefe2f3cfa96a235495242c4db98371
Paatogodui1 <akofaamaprecious@gmail.com>
2024-10-15 17:17:08 +0000

    Submitting Q1

---
+++ Bash_Scripting/install_packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#To update package list, use command: sudo apt update
+#This script installs fortune
+sudo apt install fortune
+#This just installs cowsay
+sudo apt install cowsay
+#The line below shows the command to install cmatrix
+sudo apt install cmatrix
