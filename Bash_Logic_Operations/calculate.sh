d7e5cec4ef9bcc57cfb3e6896bc280cdcd536ccd
Paatogodui1 <akofaamaprecious@gmail.com>
2024-10-21 12:34:37 +0000

    Q5

---
+++ Bash_Logic_Operations/calculate.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+Number1=$1
+Number2=$2
+Number3=$3
+
+Sum=$(($1 + $2))
+echo "The sum of $1 and $2 is: $Sum."
+
+Product=$(($Sum * $3))
+echo "The sum multiplied by $3 is: $Product."
