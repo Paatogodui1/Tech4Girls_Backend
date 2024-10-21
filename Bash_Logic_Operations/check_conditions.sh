2b392181c7c1be28145e48d0c2b58b9084577100
Paatogodui1 <akofaamaprecious@gmail.com>
2024-10-21 12:17:58 +0000

    Q3

--- Bash_Logic_Operations/check_conditions.sh
+++ Bash_Logic_Operations/check_conditions.sh
@@ -2,11 +2,12 @@
 Number1=$1
 Nunmber2=$2
 
-if (( $1 -gt 10 && $2 -gt 10 ))
+if [[ $1 -gt 10 && $2 -gt 10 ]];
 then 
-echo "Both numbers are greater than 1o"
-elif (( $1 -gt 10 || $2 -gt 10 ))
-echo "At least one number is greater than 10" 
+ echo "Both numbers are greater than 10"
+elif [[ $Number1 -gt 10 || $Number2 -gt 10 ]];
+then
+ echo "Atleast one number is greater than 10" 
 else
-echo "Neither number is greater than 10"
+ echo "Neither number is greater than 10"
 fi
---
+++ Bash_Logic_Operations/countdown.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+Number=21
+while [[ $Number -le 22 ]]
+do 
+echo "$Number"
+Number=$(($Number-1))
+done
