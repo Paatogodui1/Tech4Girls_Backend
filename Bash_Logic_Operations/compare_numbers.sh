ec19995b4b69eadad36286b88302ab50731b62c8
Paatogodui1 <akofaamaprecious@gmail.com>
2024-10-21 12:48:39 +0000

    Q6

---
+++ Bash_Logic_Operations/compare_numbers.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+Numberfirst=$1
+Numbersecond=$2
+if [[ $1 -gt $2 ]]
+then 
+ echo "$1 is greater than $2"
+elif [[ $1 -lt $2 ]]
+then 
+ echo "$1 is less than $2"
+else
+ echo "$1 is equal to $2"
+fi
