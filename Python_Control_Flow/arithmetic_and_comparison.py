032f088a70c57f2a1b671337896b46b423e9a994
Paatogodui1 <akofaamaprecious@gmail.com>
2024-10-28 15:32:46 +0000

    Assignmnet8 Q6

---
+++ Python_Control_Flow/arithmetic_and_comparison.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+x = 50
+y = 25
+
+print(f' The diiference between {x} and {y} is {x-y}')
+
+print(f' The sum of {x} and {y} is {x+y}')
+
+print(f' The product of {x} and {y} is {x*y}')
+
+print(f'{x} divided by {y} is {x/y}')
+
+print(f'{x} divided by {y} gives {x//y}')
+
+print(f' The power of {x} to  {y} is {x**y}')
+
+print(f' Dividing {x} and {y} gives {x % y}')
+
+if x==y:
+    print(f'{x} and {y} are equal')
+else:
+    print(f'{x} is not equal to {y}')
+
+if x != y:
+    print('This is true')
+else:
+    print('This is False')
+
+if x > y:
+    print(f'This is true')
+
+if x < y:
+    print(f'That is correct')
+else:
+    print(f'That is incorrect')
+
+if x >= y:
+    print(f'This is true')
+
+if x <= y:
+    print(f'That is correct')
+else:
+    print(f'That is incorrect')
