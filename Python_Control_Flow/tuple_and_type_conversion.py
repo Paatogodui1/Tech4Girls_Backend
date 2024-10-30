3a8c0c337bec28f095c99b995cfcf3bceda63f15
Paatogodui1 <akofaamaprecious@gmail.com>
2024-10-30 15:01:50 +0000

    Assignmnet8 Task4

--- Python_Control_Flow/tuple_and_type_conversion.py
+++ Python_Control_Flow/tuple_and_type_conversion.py
@@ -4,13 +4,21 @@ print(Surnames[0])
 print(Surnames[4])
 print(Surnames.count('Gbemu'))
 print(Surnames.index('Agorkpa'))
+
 Int = 7
-Int = float(Int)
-print(Int)
-Int = int(Int)
-print(Int)
-Int = str(Int)
+
+#Converting to float
+Int1 = float(Int)
 print(Int)
 
+#Converting to integer
+Int2 = int(Int1)
+print(Int)
+
+#Converting to string
+Int3 = str(Int2)
+print(Int)
+
+#Join()
 List_of_strings = ['Precious', 'Akofa', 'Ama', 'Togodui']
 print(' '.join(List_of_strings))
